1c4d7957a24f4be70512926c8ef7067aa9e37586
matheusarmando199 <matheus.narutofam@gmail.com>
2022-05-18 01:36:56 -0300

    projeto

---
+++ src/app/arquivo.ts
@@ -0,0 +1,35 @@
+
+    //Criando um array de objetos JSON.
+        const Alunos = [{ 
+        Nome: "Matheus Armando Henrique Da Silva",
+        RU: "3451495",
+        Curso: "Análise e Desenvolvimento de Sistemas",
+        DataNascimento: "26/03/1999" 			
+    },
+    {
+        Nome: "Mario Ferreira da Silva Nascimento",
+        RU: "1234567",
+        Curso: "Análise e Desenvolvimento de Sistemas",
+        DataNascimento: "12/09/1997" 			
+    },
+    {
+        Nome: "Felipe Novais Mauricio",
+        RU: "1243567",
+        Curso: "Análise e Desenvolvimento de Sistemas",
+        DataNascimento: "10/06/2001" 			
+    },
+    {
+        Nome: "Bruno Andrade Felipe Neto",
+        RU: "1243589",
+        Curso: "Análise e Desenvolvimento de Sistemas",
+        DataNascimento: "21/05/1992" 			
+    },
+    {
+        Nome: "Cloves Basilio",
+        RU: "3231495",
+        Curso: "Psicologia",
+        DataNascimento: "06/06/1996" 			
+    }];
+
+    export { Alunos }; // exportando a constante para o componente
+  
